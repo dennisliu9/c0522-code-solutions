51928dd337def2b8d294f686787d0df730388c37
Dennis Liu <dennis.liu9@gmail.com>
2022-05-16 17:26:14 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,27 @@
+var fullName;
+var isCool;
+var totalPets;
+
+fullName = 'Dennis Liu';
+isCool = false;
+totalPets = 0;
+
+console.log('My full name is ', fullName);
+console.log('Am I cool? My friends would say ', isCool);
+console.log('The number of pets I have: ', totalPets);
+
+console.log('type of fullName: ', typeof fullName);
+console.log('type of isCool: ', typeof isCool);
+console.log('type of totalPets: ', typeof totalPets);
+
+var unknown;
+unknown = null;
+
+console.log('value of unknown: ', unknown);
+console.log('type of unknown: ', typeof unknown);
+
+var never;
+never = undefined;
+
+console.log('value of never: ', never);
+console.log('type of never: ', typeof never);
