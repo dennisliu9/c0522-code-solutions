b87d0606d3e8be4e9f88f362bede8d21e4e26f05
Dennis Liu <dennis.liu9@gmail.com>
2022-05-17 19:00:18 +0000

    Revised naming to be less redundant, caught some naming inconsistencies

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,41 +2,41 @@ var orderHistory = [
   {
     orderInfo: {
       orderNumber: '114-3941689-8772232',
-      orderDetailsLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/details',
-      orderInvoiceLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/invoice',
-      orderPlacedDate: '2020-08-04',
+      detailsLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/details',
+      invoiceLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/invoice',
+      placedDate: '2020-08-04',
       orderTotal: 34.00, // Maybe this should be nested further to include currency
-      orderShipTo: {
+      shipTo: {
         shipToName: 'JS Masher',
         shipToLink: 'https://www.amazon.com/my-addresses/js-masher'
       },
       orderDelivery: {
-        orderStatus: 'Delivered',
-        orderDeliveryDate: '2020-08-08', // Holds estimated delivery date until actually delivered
+        status: 'Delivered',
+        deliveryDate: '2020-08-08', // Holds estimated delivery date until actually delivered
         note: null
       },
       getProductSupport: {
         hasEligibleItem: false,
         productSupportLink: null
       },
-      writeProductReview: 'https://www.amazon.com/my-orders/114-3941689-8772232/write-review'
+      writeProductReviewLink: 'https://www.amazon.com/my-orders/114-3941689-8772232/write-review'
     },
     orderItems: [
       {
-        itemName: 'Javascript for inpatient programmers',
-        itemUrl: 'https://www.amazon.com/products/javascript-for-inpat.html',
+        name: 'Javascript for inpatient programmers',
+        itemLink: 'https://www.amazon.com/products/javascript-for-inpat.html',
         itemImg: 'https://www.amazon.com/products/javascript-for-inpat.jpg',
-        itemType: {
+        type: {
           // Potentially more types of items that need special display values
           isBook: true,
           bookAuthor: 'Rauschmayer, Dr. Axel'
         },
-        itemReturnInfo: {
+        returnInfo: {
           isReturnWindowClosed: true,
           lastReturnDate: '2020-09-07'
         },
         itemPrice: 31.55, // Maybe should be nested further in include pre- and post- sales prices?
-        itemBuyAgainLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/item-0/buy-again.html',
+        buyAgainLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/item-0/buy-again.html',
         itemViewLink: 'https:www.amazon.com/my-orders/114-3941689-8772232/details/item-0/view-detail.html'
       }
     ]
@@ -44,11 +44,11 @@ var orderHistory = [
   {
     orderInfo: {
       orderNumber: '113-9984268-1280257',
-      orderDetailsLink: 'url-like-above',
-      orderInvoiceLink: 'url-like-above',
-      orderPlacedDate: '2020-07-19',
+      detailsLink: 'url-like-above',
+      invoiceLink: 'url-like-above',
+      placedDate: '2020-07-19',
       orderTotal: 44.53,
-      orderShipTo: {
+      shipTo: {
         shipToName: 'JS Masher',
         shipToLink: 'url-like-above'
       },
@@ -61,23 +61,23 @@ var orderHistory = [
         hasEligibleItem: false,
         productSupportLink: null
       },
-      writeProductReview: 'https://www.amazon.com/my-orders/113-9984268-1280257/write-review'
+      writeProductReviewLink: 'https://www.amazon.com/my-orders/113-9984268-1280257/write-review'
     },
     orderItems: [
       {
-        itemName: 'The Timeless Way of Building',
-        itemUrl: 'https://www.amazon.com/products/the-timeless-way-o.html',
+        name: 'The Timeless Way of Building',
+        itemLink: 'https://www.amazon.com/products/the-timeless-way-o.html',
         itemImg: 'url-like-above',
-        itemType: {
+        type: {
           isBook: true,
           bookAuthor: 'Alexander, Christopher'
         },
-        itemReturnInfo: {
+        returnInfo: {
           isReturnWindowClosed: true,
           lastReturnDate: '2020-08-19'
         },
         itemPrice: 41.33,
-        itemBuyAgainLink: 'url-like-above',
+        buyAgainLink: 'url-like-above',
         itemViewLink: 'url-like-above'
       }
     ]
@@ -85,11 +85,11 @@ var orderHistory = [
   {
     orderInfo: {
       orderNumber: '114-2875557-9059409',
-      orderDetailsLink: 'url-like-above',
-      orderInvoiceLink: 'url-like-above',
-      orderPlacedDate: '2020-07-04',
+      detailsLink: 'url-like-above',
+      invoiceLink: 'url-like-above',
+      placedDate: '2020-07-04',
       orderTotal: 17.22,
-      orderShipTo: {
+      shipTo: {
         shipToName: 'JS Masher',
         shipToLink: 'https://www.amazon.com/my-addresses/js-masher'
       },
@@ -102,23 +102,23 @@ var orderHistory = [
         hasEligibleItem: false,
         productSupportLink: null
       },
-      writeProductReview: 'https://www.amazon.com/my-orders/114-2875557-9059409/write-review'
+      writeProductReviewLink: 'https://www.amazon.com/my-orders/114-2875557-9059409/write-review'
     },
     orderItems: [
       {
-        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-        itemUrl: 'https://www.amazon.com/products/gamecube-controller-adapter-super-sma.html',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemLink: 'https://www.amazon.com/products/gamecube-controller-adapter-super-sma.html',
         itemImg: 'url-like-above',
-        itemType: {
+        type: {
           isBook: false,
           bookAuthor: null
         },
-        itemReturnInfo: {
+        returnInfo: {
           isReturnWindowClosed: true,
           lastReturnDate: '2020-08-05'
         },
         itemPrice: 15.98,
-        itemBuyAgainLink: 'url-like-above',
+        buyAgainLink: 'url-like-above',
         itemViewLink: 'url-like-above'
       }
     ]
@@ -126,11 +126,11 @@ var orderHistory = [
   {
     orderInfo: {
       orderNumber: '113-2883177-2648248',
-      orderDetailsLink: 'url-like-above',
-      orderInvoiceLink: 'url-like-above',
-      orderPlacedDate: '2020-07-03',
+      detailsLink: 'url-like-above',
+      invoiceLink: 'url-like-above',
+      placedDate: '2020-07-03',
       orderTotal: 138.93,
-      orderShipTo: {
+      shipTo: {
         shipToName: 'JS Masher',
         shipToLink: 'https://www.amazon.com/my-addresses/js-masher'
       },
@@ -143,39 +143,39 @@ var orderHistory = [
         hasEligibleItem: true,
         productSupportLink: 'https://www.amazon.com/my-orders/113-2883177-2648248/get-product-support'
       },
-      writeProductReview: 'https://www.amazon.com/my-orders/113-2883177-2648248/write-review'
+      writeProductReviewLink: 'https://www.amazon.com/my-orders/113-2883177-2648248/write-review'
     },
     orderItems: [
       {
-        itemName: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-        itemUrl: 'https://www.amazon.com/products/gamecube-controller---super-smash-bro.html',
+        name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemLink: 'https://www.amazon.com/products/gamecube-controller---super-smash-bro.html',
         itemImg: 'url-like-above',
-        itemType: {
+        type: {
           isBook: false,
           bookAuthor: null
         },
-        itemReturnInfo: {
+        returnInfo: {
           isReturnWindowClosed: true,
           lastReturnDate: '2020-08-04'
         },
         itemPrice: 94.95,
-        itemBuyAgainLink: 'url-like-above',
+        buyAgainLink: 'url-like-above',
         itemViewLink: 'url-like-above'
       },
       {
-        itemName: 'The Art of SQL',
-        itemUrl: 'https://www.amazon.com/products/the-art-of-sql.html',
+        name: 'The Art of SQL',
+        itemLink: 'https://www.amazon.com/products/the-art-of-sql.html',
         itemImg: 'url-like-above',
-        itemType: {
+        type: {
           isBook: true,
           bookAuthor: 'Faroult, Stephane'
         },
-        itemReturnInfo: {
+        returnInfo: {
           isReturnWindowClosed: true,
           lastReturnDate: '2020-08-04'
         },
         itemPrice: 33.99,
-        itemBuyAgainLink: 'url-like-above',
+        buyAgainLink: 'url-like-above',
         itemViewLink: 'url-like-above'
       }
     ]
