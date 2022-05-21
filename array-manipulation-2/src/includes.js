a909e3351ecf124394fdbebd69cd1b443fa2c99a
Dennis Liu <dennis.liu9@gmail.com>
2022-05-21 00:45:25 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,17 @@
 /* exported includes */
+/*
+Loop through array
+  If the value at the current index === value
+    return true
+  return false
+
+(this should work even on empty arrays)
+*/
+function includes(array, value) {
+  for (var arrayIdx = 0; arrayIdx < array.length; arrayIdx++) {
+    if (array[arrayIdx] === value) {
+      return true;
+    }
+  }
+  return false;
+}
