d400f94154b1c61c278c9a5bf3963295c6808100
Dennis Liu <dennis.liu9@gmail.com>
2022-05-23 17:49:42 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+console.log('hello, world');
+
+// Could also put the output of document.querySelector into variables
+// Did it this way for more practice writing it out.
+console.log(document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+console.log(document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log(document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('#example-link'));
