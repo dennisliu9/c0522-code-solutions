0ff827bfb22cb297b24df4f1c788ecc9e5e46225
Dennis Liu <dennis.liu9@gmail.com>
2022-05-24 23:28:06 +0000

    Completed object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,54 +1,141 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 /*
-potentially helpful methods from Lodash:
-  _.sample(collection)
-  _.shuffle(collection)
-  _.max(array)
-  _.takeRight(array, [n=1])
+// players looks like ['Alice', 'Bob', 'Cathy', 'David']
+// cardsPerHand is an int like 2
 
-  function myFunc(players, cardsPerHand)
-    // playerNames looks like ['Alice', 'Bob', 'Cathy', 'David']
-    // cardsPerHand is an int like 2
-    // set up players and cards for play
-    players = [] (collection?)
-    for (i=0, i<playerNames.length, i++)
-      players[i] = {name: playerNames[i], hand: []}
-
+// set up players and cards for play
+Create a variable with an empty array to hold the player objects (playerProfiles)
+Loop through the input array players and create corresponding objects, pushing them into playerProfiles
+  for (i=0, i<playerNames.length, i++)
+    players[i] = {name: playerNames[i], hand: []}
+Set up the card deck by looping through all ranks and all suits, pushing them into an array for the deck
+  Create variables
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     suits = ['club', 'diamond', 'heart', 'spade']
-    cardDeck = [] (collection?)
+    cardDeck = []
+  Loop through both ranks and suits, nested
     for (i=0, i < suits.length, i++)
       for (j=0, j<ranks.length, j++)
         cardDeck.push({rank: ranks[i], suit: suits[j]})
-    _.shuffle(cardDeck)
-    // deal cards
-    function for dealing cards
-      to an array of objects players (name and hand properties)
-      number of cards per person
-      cardDeck at the current moment
-    // score cards and assign score to players
-    function for scoring hands
+  Shuffle the card deck using _.shuffle() from Lodash
+    cardDeck = _.shuffle(cardDeck)
 
-    // check for winners
-    while loop until one winner
-      if tie, call deal cards with just the tied winners and 1 card
+// deal cards to players
+Write function for dealing cards, taking in player objects, cards per player, and a card deck
+  Determine number of deal iterations (handing one card to one player) by
+    multiplying number of players by number of cards per player (dealIters)
+  Create a loop that loops from 0 -> <dealIters
+    .pop a card from the deck and push it into the hand property of the player object
+    Access the same n number of players in order by using dealIdx % playersOjbectsArray.length
 
-  Create a set for the 4 player objects
-  Create a set for the 52 card decks
-    Loop through the 4 suits
-      Loop through the 13 ranks
-        Create objects and append to the deck set
-  Create a score lookup object
-  Shuffle the deck set
-  Loop through the players twice
-    // generalization: 2 cards * 4 players = 8 rounds. On a given round, give the card to idx (0 -> 8-1) % 4 (players)
-    Each iteration, use something like .takeRight or .pop to give a card to the player's hand
+// score cards and assign score to players
+Create an object that stores the card rank as the keys and the corresponding score as the value (cardScoring)
+Write a function for scoring every player's hand that takes an array of players objects and a card scoring guide
+  Loop through each profile
+    Create a variable to hold the score of the hand during the loop, initialized to 0 (handScore)
+    Loop through the hand (an array of "cards")
+      For each card, use the rank property to access the value in cardScoring for that card
+      Add this score to handScore
+    Assign the player's handScore as a new key to the player object
 
+// find winners
+Write a function find the winner(s) of an array of player objects with the handScore property
+  Create an empty array to store the winning player objects (winnersArray)
+  Create a variable to hold the top score, initialized to 0 (var highestScore = 0)
+  !! potentially optimize here
+  Loop through the array with the players objects
+    Check if the hand score of the current player is higher than the highestScore
+      If so, set highestScore = current player's hand score
+  Loop through the array with the players objects again
+    Check if the player's hand score === highest score
+      If so, push that player object to winnersArray
+
+Call the function and store the return in a new variable (gameWinners)
+
+// Check for ties
+While gameWinners has more than one value inside (more than one winning player)
+  Call the dealCardsFromDeck function again with gameWinners as the players, dealing 1 card, with the same card deck
+  Call the addHandScoresToProfiles function again with gameWinners as the players, and the same cardScoring object
+  Call the findWinners function again on gameWinners and assign the return back to gameWinners
+When the loop stops, there is only one winner in gameWinners, accessible at gameWinners[0]
+
+log the winners to see results
 */
+function playGame(players, cardsPerHand) {
+  // game setup
+  var playerProfiles = [];
+  for (var playerIdx = 0; playerIdx < players.length; playerIdx++) {
+    playerProfiles.push({ name: players[playerIdx], hand: [] });
+  }
+  var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  var suits = ['club', 'diamond', 'heart', 'spade'];
+  var cardDeck = [];
+  for (var suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (var rankIdx = 0; rankIdx < ranks.length; rankIdx++) {
+      cardDeck.push({ rank: ranks[rankIdx], suit: suits[suitIdx] });
+    }
+  }
+  cardDeck = _.shuffle(cardDeck);
 
-var myArray = [1, 2, 3];
-var taken = _.takeRight(myArray, 1);
+  // deal cards to players
+  function dealCardsFromDeck(playersObjectsArray, cardsPerPlayer, cardDeck) {
+    // assuming we will always have enough cards, otherwise do an if check
+    var dealIters = playersObjectsArray.length * cardsPerPlayer;
+    for (var dealIdx = 0; dealIdx < dealIters; dealIdx++) {
+      var cardDrawn = cardDeck.pop();
+      playersObjectsArray[dealIdx % playersObjectsArray.length].hand.push(cardDrawn);
+    }
+  }
+  dealCardsFromDeck(playerProfiles, cardsPerHand, cardDeck);
 
-console.log('myArray: ', myArray);
-console.log('taken: ', taken);
+  // score each player's hand
+  var cardScoring = { A: 11, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, J: 10, Q: 10, K: 10 };
+  function addHandScoresToProfiles(playersObjectsArray, cardScoring) {
+    for (var profileIdx = 0; profileIdx < playersObjectsArray.length; profileIdx++) {
+      var handScore = 0;
+      var currentHand = playersObjectsArray[profileIdx].hand;
+      for (var handIdx = 0; handIdx < currentHand.length; handIdx++) {
+        handScore += cardScoring[currentHand[handIdx].rank];
+      }
+      playersObjectsArray[profileIdx].handScore = handScore;
+    }
+  }
+  addHandScoresToProfiles(playerProfiles, cardScoring);
+
+  // determine a winner
+  function findWinners(playersObjectsArray) {
+    var winnersArray = [];
+    var highestScore = 0;
+    // There is probably something in Lodash to simplify this
+    for (var playerIdx = 0; playerIdx < playersObjectsArray.length; playerIdx++) {
+      if (playersObjectsArray[playerIdx].handScore > highestScore) {
+        highestScore = playersObjectsArray[playerIdx].handScore;
+      }
+    }
+    for (playerIdx = 0; playerIdx < playersObjectsArray.length; playerIdx++) {
+      if (playersObjectsArray[playerIdx].handScore === highestScore) {
+        winnersArray.push(playersObjectsArray[playerIdx]);
+      }
+    }
+    return winnersArray;
+  }
+  var gameWinners = findWinners(playerProfiles);
+
+  // Check for ties
+  while (gameWinners.length > 1) {
+    dealCardsFromDeck(gameWinners, 1, cardDeck);
+    addHandScoresToProfiles(gameWinners, cardScoring);
+    gameWinners = findWinners(gameWinners);
+  }
+
+  // Announce winners
+  console.log('The winner is: ', gameWinners[0].name, '!');
+  console.log('---');
+  console.log('player profiles: ', playerProfiles);
+}
+
+// example
+var samplePlayers = ['Blossom', 'Bubbles', 'Buttercup', 'Prof. Utonium'];
+var sampleCardsPerHand = 2;
+playGame(samplePlayers, sampleCardsPerHand);
