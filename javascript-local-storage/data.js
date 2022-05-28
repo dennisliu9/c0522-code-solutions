6d951ba65ebdf16d20bf76267f621307f943d121
Dennis Liu <dennis.liu9@gmail.com>
2022-05-28 00:05:00 +0000

    Updated code to include event as a parameter in the anonymous function

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -7,7 +7,7 @@ if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
 
-window.addEventListener('beforeunload', function () {
+window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
 });
