a3f7dc1bcaee98f8e69f9ade88141a1b90b0eccb
Dennis Liu <dennis.liu9@gmail.com>
2022-06-07 18:29:01 +0000

    Completed javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var myObj = new Object();
+console.log('value of myObj: ', myObj);
+console.log('typeof myObj: ', typeof myObj);
+
+var myArray = new Array();
+console.log('value of myArray: ', myArray);
+console.log('typeof myArray: ', typeof myArray);
+
+var myFunc = new Function();
+console.log('value of myFunc: ', myFunc);
+console.log('typeof myFunc: ', typeof myFunc);
+
+var myStr = new String();
+console.log('value of myStr: ', myStr);
+console.log('typeof myStr: ', typeof myStr);
+
+var myNum = new Number();
+console.log('value of myNum: ', myNum);
+console.log('typeof myNum: ', typeof myNum);
+
+var myBool = new Boolean();
+console.log('value of myBool: ', myBool);
+console.log('typeof myBool: ', typeof myBool);
